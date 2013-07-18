f31745e61410228833a153f889ac52c675f00999
Tijee <thomas.gallinari@gmail.com>
2013-07-18 08:48:39 +0200

    Add search view

--- velibnroses/velibnroses/MapController.h
+++ velibnroses/velibnroses/MapController.h
@@ -10,11 +10,22 @@
 #import <CoreLocation/CoreLocation.h>
 #import "CoreLocationHelper.h"
 
-@interface MapController : UIViewController <CoreLocationHelperDelegate> {
+@interface MapController : UIViewController <CoreLocationHelperDelegate, UITextFieldDelegate> {
     @private CLLocationCoordinate2D currentLocation;
 }
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UIView *searchView;
+@property (weak, nonatomic) IBOutlet UITextField *departureField;
+@property (weak, nonatomic) IBOutlet UITextField *arrivalField;
+@property (weak, nonatomic) IBOutlet UITextField *bikeField;
+@property (weak, nonatomic) IBOutlet UIStepper *bikeStepper;
 @property (nonatomic, retain) CoreLocationHelper *locationHelper;
+
+- (IBAction)bikesChanged:(UIStepper *)stepper;
+- (IBAction)toggleSearchView:(id)sender;
+- (IBAction)useMyLocationAsDeparture:(id)sender;
+- (IBAction)useMyLocationAsArrival:(id)sender;
+- (IBAction)search:(id)sender;
 
 @end
--- velibnroses/velibnroses/MapController.m
+++ velibnroses/velibnroses/MapController.m
@@ -19,14 +19,33 @@
 @end
 
 
-@implementation MapController
+@implementation MapController {
+    BOOL _searchViewVisible;
+    BOOL _searching;
+    CLLocation *_departureLocation;
+    CLLocation *_arrivalLocation;
+}
 
 @synthesize mapView;
+@synthesize searchView;
+@synthesize departureField;
+@synthesize arrivalField;
+@synthesize bikeField;
+@synthesize bikeStepper;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     self.mapView.showsUserLocation = YES;
+    _searchViewVisible = NO;
+    
+    CGRect searchFrame = self.searchView.frame;
+    CGRect mapFrame = self.mapView.frame;
+    searchFrame.origin.y = -searchFrame.size.height;
+    mapFrame.origin.y = 0;
+    self.searchView.frame = searchFrame;
+    self.mapView.frame = mapFrame;
     
     self.locationHelper = [[CoreLocationHelper alloc] init];
 	self.locationHelper.delegate = self;
@@ -63,6 +82,137 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)bikesChanged:(UIStepper *)stepper {
+    // default min is 0 because we start with 0, now set the min to 1
+    stepper.minimumValue = 1;
+    self.bikeField.text = [NSString stringWithFormat:@"%d", (int) stepper.value];
+}
+
+- (IBAction)toggleSearchView:(id)sender {
+    [self toggleSearchView];
+}
+
+- (IBAction)useMyLocationAsDeparture:(id)sender {
+    CLLocationCoordinate2D userLocation = self.mapView.userLocation.coordinate;
+    CLLocation *location = [[CLLocation alloc] initWithLatitude:userLocation.latitude longitude:userLocation.longitude];
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        if (error != nil) {
+            self.departureField.text = [NSString stringWithFormat:@"%f,%f", userLocation.latitude, userLocation.longitude];
+        } else {
+            self.departureField.text = [[[(CLPlacemark *)[placemarks objectAtIndex:0] addressDictionary] valueForKey:@"FormattedAddressLines"] componentsJoinedByString:@", "];
+        }
+    }];
+}
+
+- (IBAction)useMyLocationAsArrival:(id)sender {
+    CLLocationCoordinate2D userLocation = self.mapView.userLocation.coordinate;
+    CLLocation *location = [[CLLocation alloc] initWithLatitude:userLocation.latitude longitude:userLocation.longitude];
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        if (error != nil) {
+            self.arrivalField.text = [NSString stringWithFormat:@"%f,%f", userLocation.latitude, userLocation.longitude];
+        } else {
+            self.arrivalField.text = [[[(CLPlacemark *)[placemarks objectAtIndex:0] addressDictionary] valueForKey:@"FormattedAddressLines"] componentsJoinedByString:@", "];
+        }
+    }];
+}
+
+- (IBAction)search:(id)sender {
+    [self.view endEditing:YES];
+    if (self.departureField.text.length == 0) {
+        [[[UIAlertView alloc] initWithTitle:nil message:NSLocalizedString(@"missing_departure", @"") delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", @"") otherButtonTitles:nil] show];
+    } else if (self.arrivalField.text.length == 0) {
+        [[[UIAlertView alloc] initWithTitle:nil message:NSLocalizedString(@"missing_arrival", @"") delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", @"") otherButtonTitles:nil] show];
+    } else if (self.bikeField.text.length == 0 || [self.bikeField.text intValue] <= 0) {
+        [self.view endEditing:YES];
+        [[[UIAlertView alloc] initWithTitle:nil message:NSLocalizedString(@"missing_bikes", @"") delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", @"") otherButtonTitles:nil] show];
+    } else {
+        [self.view endEditing:YES];
+        [self toggleSearchView];
+        _departureLocation = nil;
+        _arrivalLocation = nil;
+        _searching = YES;
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+        CLGeocoder *departureGeocoder = [[CLGeocoder alloc] init];
+        CLGeocoder *arrivalGeocoder = [[CLGeocoder alloc] init];
+        [departureGeocoder geocodeAddressString:self.departureField.text inRegion:nil completionHandler:^(NSArray *placemarks, NSError *error) {
+            if (_searching) {
+                if (error != nil) {
+                    _searching = NO;
+                    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+                    [[[UIAlertView alloc] initWithTitle:nil message:NSLocalizedString(@"departure_not_found", @"") delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", @"") otherButtonTitles:nil] show];
+                } else {
+                    _departureLocation = [[placemarks objectAtIndex:0] location];
+                    if (_arrivalLocation != nil) {
+                        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+                        [self searchWithDeparture:_departureLocation andArrival:_arrivalLocation withNumberOfBikes:[self.bikeField.text intValue]];
+                    }
+                }
+            }
+        }];
+        [arrivalGeocoder geocodeAddressString:self.arrivalField.text inRegion:nil completionHandler:^(NSArray *placemarks, NSError *error) {
+            if (_searching) {
+                if (error != nil) {
+                    _searching = NO;
+                    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+                    [[[UIAlertView alloc] initWithTitle:nil message:NSLocalizedString(@"arrival_not_found", @"") delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", @"") otherButtonTitles:nil] show];
+                } else {
+                    _arrivalLocation = [[placemarks objectAtIndex:0] location];
+                    if (_departureLocation != nil) {
+                        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+                        [self searchWithDeparture:_departureLocation andArrival:_arrivalLocation withNumberOfBikes:[self.bikeField.text intValue]];
+                    }
+                }
+            }
+        }];
+    }
+}
+
+- (void)toggleSearchView {
+    _searchViewVisible = !_searchViewVisible;
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect searchFrame = self.searchView.frame;
+        CGRect mapFrame = self.mapView.frame;
+        if (_searchViewVisible) {
+            searchFrame.origin.y = 0;
+            mapFrame.origin.y = searchFrame.size.height;
+        } else {
+            searchFrame.origin.y = -searchFrame.size.height;
+            mapFrame.origin.y = 0;
+            [self.view endEditing:YES];
+        }
+        self.searchView.frame = searchFrame;
+        self.mapView.frame = mapFrame;
+    }];
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    if (textField == self.bikeField) {
+        [self.view endEditing:YES];
+        return NO;
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField == self.departureField) {
+        [self.arrivalField becomeFirstResponder];
+    } else if (textField == self.arrivalField) {
+        [self.view endEditing:YES];
+    }
+    return YES;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
+- (void)searchWithDeparture:(CLLocation *)departure andArrival:(CLLocation *)arrival withNumberOfBikes:(int)bikes {
+    NSLog(@"%f,%f -> %f,%f (%d)", departure.coordinate.latitude, departure.coordinate.longitude, arrival.coordinate.latitude, arrival.coordinate.longitude, bikes);
+    // TODO Search stations
 }
 
 @end
